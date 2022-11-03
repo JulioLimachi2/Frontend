802827d49c3b8108f28b73d24ffe8882d1e44247
perueduca91 <perueduca91@minedu.gob.pe>
2022-11-02 20:22:19 -0500

    add data table

--- src/app/pages/administration-risk/impact/impact.component.ts
+++ src/app/pages/administration-risk/impact/impact.component.ts
@@ -12,4 +12,25 @@ export class ImpactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  getDataSource(): any[] {
+    return [
+      {color: '#FF0000', categoria: 'Extremo', valor:'5', negativo: 'Se presentó una o mas veces a la semana',positivo: 'Se presentó una o mas veces a la semana' },
+      { color: '#ff8000',categoria: 'Mayor', valor:'4',negativo: 'Se presentó una o mas veces al año' ,positivo: 'Se presentó una o mas veces a la semana'},
+      { color: '#ffff00',categoria: 'Moderado',valor:'3', negativo: 'Se presentó una vez al año',positivo: 'Se presentó una o mas veces a la semana' },
+      { color: '#B2D13F',categoria: 'Moderado', valor:'2',negativo: 'Ha sucedido una vez en la historia de la institución',positivo: 'Se presentó una o mas veces a la semana' },
+      { color: '#36FF33',categoria: 'Insignificativo',valor:'1', negativo: 'Nunca se ha presentado',positivo: 'Se presentó una o mas veces a la semana' },
+    ];
+  }
+
+
+  getColumns():  any[] {
+    return [
+      { caption: 'Categoría', field: 'categoria' },
+      { caption: 'Valor', field: 'valor' },
+      { caption: 'Negativo', field: 'negativo' },  
+       { caption: 'Positivo', field: 'positivo' }
+    ];
+  };
+
 }
--- src/app/pages/administration-risk/treatment/treatment.component.ts
+++ src/app/pages/administration-risk/treatment/treatment.component.ts
@@ -11,5 +11,21 @@ export class TreatmentComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  getDataSource(): any[] {
+    return [
+      {color: '#FF0000', categoria: '17 a mas',categoria2: 'Extremo' },
+      { color: '#ff8000',categoria: 'Entre 11 y 16',categoria2: 'Alto'},
+      { color: '#ffff00',categoria: 'Entre 5 y 10',categoria2: 'Medio' },
+      { color: '#B2D13F',categoria: 'Entre 3 y 4',categoria2: 'Ligero'},
+      { color: '#36FF33',categoria: 'Hasta 2',categoria2: 'Bajo'},
+    ];
+  }
 
+
+  getColumns():  any[] {
+    return [
+      { caption: 'Nombre', field: 'categoria' },
+      { caption: 'Código', field: 'categoria2' },
+    ];
+  };
 }
