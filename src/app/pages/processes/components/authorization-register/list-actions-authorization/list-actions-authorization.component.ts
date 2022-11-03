92e0c4804424b9384d5989d3d6bfd108221e2864
JulioLimachi2 <julio.limachi@latitudde.com>
2022-11-02 21:15:03 -0500

    agregado de filtro en list action-authorization

--- src/app/pages/processes/components/authorization-register/list-actions-authorization/list-actions-authorization.component.ts
+++ src/app/pages/processes/components/authorization-register/list-actions-authorization/list-actions-authorization.component.ts
@@ -11,11 +11,90 @@ export class ListActionsAuthorizationComponent implements OnInit {
 
   formSearchActions: FormGroup;
   @Output() onNext: EventEmitter<boolean> = new EventEmitter();
-  displayedColumns: string[] = ['type', 'correlative', 'year', 'coordinator','area'];
+  displayedColumns: string[] = ['type', 'correlative', 'year', 'coordinator','area','state'];
   areas = [
     {
       id: '01',
       name: 'Gerencia de Informatica'
+    }
+  ];
+  dataActions = [
+    {
+      type:'Preventiva',
+      correlative: '0001',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '01'
+    },
+    {
+      type:'Oportunidad de mejora',
+      correlative: '0002',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '01'
+    },
+    {
+      type:'Preventiva',
+      correlative: '0003',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '01'
+    },
+    {
+      type:'Oportunidad de mejora',
+      correlative: '0004',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '01'
+    },
+    {
+      type:'Preventiva',
+      correlative: '0005',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '01'
+    },
+    {
+      type:'Preventiva',
+      correlative: '0006',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '01'
+    },
+    {
+      type:'Preventiva',
+      correlative: '0007',
+      year: '2015',
+      coordinator: 'Llanos Barrios Patricia',
+      area: {
+        id:'01',
+        name: 'Gerencia de Informática'
+      },
+      state: '02'
     }
   ];
 
@@ -30,65 +109,24 @@ export class ListActionsAuthorizationComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.dataSource.data = [
-      {
-        type:'Preventiva',
-        correlative: '0001',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      },
-      {
-        type:'Oportunidad de mejora',
-        correlative: '0002',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      },
-      {
-        type:'Preventiva',
-        correlative: '0003',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      },
-      {
-        type:'Oportunidad de mejora',
-        correlative: '0004',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      },
-      {
-        type:'Preventiva',
-        correlative: '0005',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      },
-      {
-        type:'Preventiva',
-        correlative: '0006',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      },
-      {
-        type:'Preventiva',
-        correlative: '0007',
-        year: '2015',
-        coordinator: 'Llanos Barrios Patricia',
-        area: 'Gerencia de Informática'
-      }
-    ];
+    this.dataSource.data = this.dataActions;
   }
 
   getYear(year: string) {
-    console.log('year', year);
+    this.formSearchActions.controls['year'].setValue(year);
   }
 
   search(){
-    
+    const filtrado = this.dataActions.filter(action => {
+      const year = this.formSearchActions.value.year ? action.year === this.formSearchActions.value.year : 1;
+      const state = this.formSearchActions.value.state ? action.state === this.formSearchActions.value.state : 1;
+      const area = this.formSearchActions.value.area ? action.area.id === this.formSearchActions.value.area : 1;
+      if(year === 1 && state === 1 && area === 1){
+        return false
+      }
+      return year && state && area;
+    });
+    this.dataSource.data = filtrado;
   }
 
   next(){
